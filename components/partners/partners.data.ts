824420ec0f982114eb5658fa0bb0fd6cd903711a
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2024-09-24 10:15:38 +0100

    :sparkles: Partners Section (#12)

---
+++ components/partners/partners.data.ts
@@ -0,0 +1,64 @@
+import { PartnersImage } from './partners.types';
+
+export const PARTNERS_IMAGES: ReadonlyArray<PartnersImage> = [
+  {
+    image: '/img/movement-labs.webp',
+    label: 'Movement Labs',
+    url: 'https://movementlabs.xyz/',
+  },
+  {
+    image: '/img/satay.webp',
+    label: 'Satay',
+    url: 'https://www.satay.finance/',
+  },
+  {
+    image: '/img/route-x.webp',
+    label: 'Route-X',
+    url: 'https://x.com/routexio',
+  },
+  {
+    image: '/img/after-math.webp',
+    label: 'After Math',
+    url: 'https://aftermath.finance/',
+  },
+  {
+    image: '/img/move-wiffrens.webp',
+    label: 'Move wiffrens',
+    url: 'https://www.movewiffrens.xyz/',
+  },
+  {
+    image: '/img/hop.webp',
+    label: 'Hop Aggregator',
+    url: 'https://hop.ag/',
+  },
+  {
+    image: '/img/up-network.webp',
+    label: 'Up Network',
+    url: 'https://www.upnetwork.xyz/',
+  },
+  {
+    image: '/img/nightly.webp',
+    label: 'Nightly',
+    url: 'https://nightly.app/',
+  },
+  {
+    image: '/img/mosaic.webp',
+    label: 'Mosaic',
+    url: 'https://www.mosaic.io/',
+  },
+  {
+    image: '/img/pyth.webp',
+    label: 'Pyth',
+    url: 'https://www.pyth.network/',
+  },
+  {
+    image: '/img/gorilla-moverz.webp',
+    label: 'Gorilla Maverz',
+    url: 'https://coinmarketcap.com/currencies/gorilla-token/',
+  },
+  {
+    image: '/img/stamp.webp',
+    label: 'Portugal Stamp',
+    url: 'https://recuperarportugal.gov.pt/',
+  },
+];
