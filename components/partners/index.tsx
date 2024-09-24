e101dec74608722d34a56cdae0a9152678891fde
Mario Batalha <mariobatalha9@gmail.com>
2024-09-24 03:25:04 +0100

    :construction: feat: work in progress

--- components/partners/index.tsx
+++ components/partners/index.tsx
@@ -7,56 +7,65 @@ import { MOCK_IMAGES } from './images';
 
 const Partners: FC = () => {
   return (
-    <Box
-      mx="l"
-      display="flex"
-      gap="0.5rem"
-      maxWidth="1440px"
-      flexDirection="column"
-    >
-      <Typography
-        pb="8xl"
-        size="large"
-        variant="title"
-        fontWeight="700"
-        color="onSurface"
-        lineHeight="100%"
-        fontSize={['2.25rem', '2.25rem', '3.5rem']}
-      >
-        Our Partners
-      </Typography>
+    <Box maxWidth="1440px" mx="auto">
       <Box
         display="flex"
-        flexWrap="wrap"
-        height="auto"
-        borderRadius="m"
+        alignItems="flex-start"
         justifyContent="center"
-        alignItems="center"
-        border="1px solid"
-        py={['m', 'm', 'm', '2xl']}
-        px={['m', 'm', 'm', '4xl']}
-        borderColor="outlineVariant"
-        nHover={{ borderColor: 'outline', boxShadow: '0 0 0.5rem #fff4' }}
+        gap="0.5rem"
+        flexDirection="column"
+        py="8xl"
+        px="xl"
       >
-        {MOCK_IMAGES.map((item) => {
-          return (
-            <Box
-              m="1rem"
-              width="auto"
-              key={unikey()}
-              cursor="pointer"
-              overflow="hidden"
-            >
-              <Link href={item.url} target="blank">
-                <img
-                  src={item.image}
-                  alt={item.label}
-                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
-                />
-              </Link>
-            </Box>
-          );
-        })}
+        <Typography
+          pb="8xl"
+          size="large"
+          variant="title"
+          fontWeight="700"
+          color="onSurface"
+          lineHeight="100%"
+          fontSize={['2.25rem', '2.25rem', '3.5rem']}
+        >
+          Our Partners
+        </Typography>
+        <Box
+          display="flex"
+          flexWrap="wrap"
+          height="auto"
+          width="100%"
+          borderRadius="m"
+          justifyContent="center"
+          alignItems="center"
+          border="1px solid"
+          py={['m', 'm', 'm', '2xl']}
+          px={['m', 'm', 'm', '4xl']}
+          borderColor="outlineVariant"
+          nHover={{ borderColor: 'outline', boxShadow: '0 0 0.5rem #fff4' }}
+        >
+          {MOCK_IMAGES.map((item) => {
+            return (
+              <Box
+                m="1rem"
+                width="auto"
+                key={unikey()}
+                cursor="pointer"
+                overflow="hidden"
+              >
+                <Link href={item.url} target="blank">
+                  <img
+                    src={item.image}
+                    alt={item.label}
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      objectFit: 'cover',
+                    }}
+                  />
+                </Link>
+              </Box>
+            );
+          })}
+        </Box>
       </Box>
     </Box>
   );
